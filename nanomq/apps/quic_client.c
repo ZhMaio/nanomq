ba1aec4b3347600c1a2650cd152d16b55a57c75f
wanghaemq <wangwei@emqx.io>
2022-04-28 17:47:16 +0800

    NEW [quic] mqtt connect request over quic is supported.

--- nanomq/apps/quic_client.c
+++ nanomq/apps/quic_client.c
@@ -54,6 +54,8 @@ connect_cb(nng_pipe p, nng_pipe_ev ev, void *arg)
 	if (rv != 0) {
 		quic_fatal("nng_sendmsg", rv);
 	}
+
+	nng_close(sock);
 }
 
 void
@@ -85,7 +87,7 @@ quic_sub_cb(void *arg)
 }
 
 struct work *
-proxy_alloc_work(nng_socket sock)
+quic_alloc_work(nng_socket sock)
 {
 	struct work *w;
 	int          rv;
@@ -176,6 +178,305 @@ Error:
     }
 }
 
+//
+// The clients's callback for stream events from MsQuic.
+//
+_IRQL_requires_max_(DISPATCH_LEVEL)
+_Function_class_(QUIC_STREAM_CALLBACK)
+QUIC_STATUS
+QUIC_API
+ClientStreamCallback(
+    _In_ HQUIC Stream,
+    _In_opt_ void* Context,
+    _Inout_ QUIC_STREAM_EVENT* Event
+    )
+{
+    switch (Event->Type) {
+    case QUIC_STREAM_EVENT_SEND_COMPLETE:
+        //
+        // A previous StreamSend call has completed, and the context is being
+        // returned back to the app.
+        //
+        free(Event->SEND_COMPLETE.ClientContext);
+        printf("[strm][%p] Data sent\n", Stream);
+        break;
+    case QUIC_STREAM_EVENT_RECEIVE:
+        //
+        // Data was received from the peer on the stream.
+        //
+        printf("[strm][%p] Data received\n", Stream);
+        break;
+    case QUIC_STREAM_EVENT_PEER_SEND_ABORTED:
+        //
+        // The peer gracefully shut down its send direction of the stream.
+        //
+        printf("[strm][%p] Peer aborted\n", Stream);
+        break;
+    case QUIC_STREAM_EVENT_PEER_SEND_SHUTDOWN:
+        //
+        // The peer aborted its send direction of the stream.
+        //
+        printf("[strm][%p] Peer shut down\n", Stream);
+        break;
+    case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
+        //
+        // Both directions of the stream have been shut down and MsQuic is done
+        // with the stream. It can now be safely cleaned up.
+        //
+        printf("[strm][%p] All done\n", Stream);
+        if (!Event->SHUTDOWN_COMPLETE.AppCloseInProgress) {
+            MsQuic->StreamClose(Stream);
+        }
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+void
+ClientSend(
+    _In_ HQUIC Connection
+    )
+{
+    QUIC_STATUS Status;
+    HQUIC Stream = NULL;
+    uint8_t* SendBufferRaw;
+    QUIC_BUFFER* SendBuffer;
+
+    //
+    // Create/allocate a new bidirectional stream. The stream is just allocated
+    // and no QUIC stream identifier is assigned until it's started.
+    //
+    if (QUIC_FAILED(Status = MsQuic->StreamOpen(Connection, QUIC_STREAM_OPEN_FLAG_NONE, ClientStreamCallback, NULL, &Stream))) {
+        printf("StreamOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    printf("[strm][%p] Starting...\n", Stream);
+
+    //
+    // Starts the bidirectional stream. By default, the peer is not notified of
+    // the stream being started until data is sent on the stream.
+    //
+    if (QUIC_FAILED(Status = MsQuic->StreamStart(Stream, QUIC_STREAM_START_FLAG_NONE))) {
+        printf("StreamStart failed, 0x%x!\n", Status);
+        MsQuic->StreamClose(Stream);
+        goto Error;
+    }
+
+	// Mqtt connect message
+	nng_msg *msg;
+	nng_mqtt_msg_alloc(&msg, 0);
+	nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
+
+	nng_mqtt_msg_set_connect_keep_alive(msg, 180);
+	nng_mqtt_msg_set_connect_clean_session(msg, true);
+
+    //
+    // Allocates and builds the buffer to send over the stream.
+    //
+    SendBufferRaw = (uint8_t*)malloc(sizeof(QUIC_BUFFER) + SendBufferLength);
+    if (SendBufferRaw == NULL) {
+        printf("SendBuffer allocation failed!\n");
+        Status = QUIC_STATUS_OUT_OF_MEMORY;
+        goto Error;
+    }
+
+	nng_mqtt_msg_encode(msg);
+	int header_len = nng_msg_header_len(msg);
+	int body_len   = nng_msg_len(msg);
+	char * header  = nng_msg_header(msg);
+	char * body    = nng_msg_body(msg);
+
+	if (header_len + body_len > SendBufferLength)
+        printf("SendBuffer Overflow!\n");
+
+	memcpy(SendBufferRaw, header, header_len);
+	memcpy(SendBufferRaw+header_len, body, body_len);
+
+    SendBuffer = (QUIC_BUFFER*)SendBufferRaw;
+    SendBuffer->Buffer = SendBufferRaw + sizeof(QUIC_BUFFER);
+    SendBuffer->Length = SendBufferLength;
+
+    printf("[strm][%p] Sending data...\n", Stream);
+
+    //
+    // Sends the buffer over the stream. Note the FIN flag is passed along with
+    // the buffer. This indicates this is the last buffer on the stream and the
+    // the stream is shut down (in the send direction) immediately after.
+    //
+    if (QUIC_FAILED(Status = MsQuic->StreamSend(Stream, SendBuffer, 1, QUIC_SEND_FLAG_FIN, SendBuffer))) {
+        printf("StreamSend failed, 0x%x!\n", Status);
+        free(SendBufferRaw);
+        goto Error;
+    }
+
+Error:
+
+    if (QUIC_FAILED(Status)) {
+        MsQuic->ConnectionShutdown(Connection, QUIC_CONNECTION_SHUTDOWN_FLAG_NONE, 0);
+    }
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+_Function_class_(QUIC_CONNECTION_CALLBACK)
+QUIC_STATUS
+QUIC_API
+ClientConnectionCallback(
+    _In_ HQUIC Connection,
+    _In_opt_ void* Context,
+    _Inout_ QUIC_CONNECTION_EVENT* Event
+    )
+{
+    switch (Event->Type) {
+    case QUIC_CONNECTION_EVENT_CONNECTED:
+        //
+        // The handshake has completed for the connection.
+        //
+        printf("[conn][%p] Connected\n", Connection);
+        ClientSend(Connection);
+        break;
+    case QUIC_CONNECTION_EVENT_SHUTDOWN_INITIATED_BY_TRANSPORT:
+        //
+        // The connection has been shut down by the transport. Generally, this
+        // is the expected way for the connection to shut down with this
+        // protocol, since we let idle timeout kill the connection.
+        //
+        if (Event->SHUTDOWN_INITIATED_BY_TRANSPORT.Status == QUIC_STATUS_CONNECTION_IDLE) {
+            printf("[conn][%p] Successfully shut down on idle.\n", Connection);
+        } else {
+            printf("[conn][%p] Shut down by transport, 0x%x\n", Connection, Event->SHUTDOWN_INITIATED_BY_TRANSPORT.Status);
+        }
+        break;
+    case QUIC_CONNECTION_EVENT_SHUTDOWN_INITIATED_BY_PEER:
+        //
+        // The connection was explicitly shut down by the peer.
+        //
+        printf("[conn][%p] Shut down by peer, 0x%llu\n", Connection, (unsigned long long)Event->SHUTDOWN_INITIATED_BY_PEER.ErrorCode);
+        break;
+    case QUIC_CONNECTION_EVENT_SHUTDOWN_COMPLETE:
+        //
+        // The connection has completed the shutdown process and is ready to be
+        // safely cleaned up.
+        //
+        printf("[conn][%p] All done\n", Connection);
+        if (!Event->SHUTDOWN_COMPLETE.AppCloseInProgress) {
+            MsQuic->ConnectionClose(Connection);
+        }
+        break;
+    case QUIC_CONNECTION_EVENT_RESUMPTION_TICKET_RECEIVED:
+        //
+        // A resumption ticket (also called New Session Ticket or NST) was
+        // received from the server.
+        //
+        printf("[conn][%p] Resumption ticket received (%u bytes):\n", Connection, Event->RESUMPTION_TICKET_RECEIVED.ResumptionTicketLength);
+        for (uint32_t i = 0; i < Event->RESUMPTION_TICKET_RECEIVED.ResumptionTicketLength; i++) {
+            printf("%.2X", (uint8_t)Event->RESUMPTION_TICKET_RECEIVED.ResumptionTicket[i]);
+        }
+        printf("\n");
+        break;
+    default:
+        break;
+    }
+    return QUIC_STATUS_SUCCESS;
+}
+
+
+//
+// Helper function to load a client configuration.
+//
+BOOLEAN
+ClientLoadConfiguration(
+    BOOLEAN Unsecure
+    )
+{
+    QUIC_SETTINGS Settings = {0};
+    //
+    // Configures the client's idle timeout.
+    //
+    Settings.IdleTimeoutMs = IdleTimeoutMs;
+    Settings.IsSet.IdleTimeoutMs = TRUE;
+
+    //
+    // Configures a default client configuration, optionally disabling
+    // server certificate validation.
+    //
+    QUIC_CREDENTIAL_CONFIG CredConfig;
+    memset(&CredConfig, 0, sizeof(CredConfig));
+    CredConfig.Type = QUIC_CREDENTIAL_TYPE_NONE;
+    CredConfig.Flags = QUIC_CREDENTIAL_FLAG_CLIENT;
+    if (Unsecure) {
+        CredConfig.Flags |= QUIC_CREDENTIAL_FLAG_NO_CERTIFICATE_VALIDATION;
+    }
+
+    //
+    // Allocate/initialize the configuration object, with the configured ALPN
+    // and settings.
+    //
+    QUIC_STATUS Status = QUIC_STATUS_SUCCESS;
+    if (QUIC_FAILED(Status = MsQuic->ConfigurationOpen(Registration, &Alpn, 1, &Settings, sizeof(Settings), NULL, &Configuration))) {
+        printf("ConfigurationOpen failed, 0x%x!\n", Status);
+        return FALSE;
+    }
+
+    //
+    // Loads the TLS credential part of the configuration. This is required even
+    // on client side, to indicate if a certificate is required or not.
+    //
+    if (QUIC_FAILED(Status = MsQuic->ConfigurationLoadCredential(Configuration, &CredConfig))) {
+        printf("ConfigurationLoadCredential failed, 0x%x!\n", Status);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+void
+quic_send()
+{
+    //
+    // Load the client configuration based on the "unsecure" command line option.
+    //
+    if (!ClientLoadConfiguration(true)) {
+        return;
+    }
+
+    QUIC_STATUS Status;
+    const char* ResumptionTicketString = NULL;
+    HQUIC Connection = NULL;
+
+    //
+    // Allocate a new connection object.
+    //
+    if (QUIC_FAILED(Status = MsQuic->ConnectionOpen(Registration, ClientConnectionCallback, NULL, &Connection))) {
+        printf("ConnectionOpen failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+    //
+    // Get the target / server name or IP from the command line.
+    //
+    const char* Target = "localhost";
+
+    printf("[conn][%p] Connecting...\n", Connection);
+
+    //
+    // Start the connection to the server.
+    //
+    if (QUIC_FAILED(Status = MsQuic->ConnectionStart(Connection, Configuration, QUIC_ADDRESS_FAMILY_UNSPEC, Target, UdpPort))) {
+        printf("ConnectionStart failed, 0x%x!\n", Status);
+        goto Error;
+    }
+
+Error:
+
+    if (QUIC_FAILED(Status) && Connection != NULL) {
+        MsQuic->ConnectionClose(Connection);
+    }
+
+}
+
 int
 client(const char *url, nng_socket *sock_ret)
 {
@@ -195,13 +496,19 @@ client(const char *url, nng_socket *sock_ret)
 	quic_open();
 
 	for (int i = 0; i < nwork; i++) {
-		works[i] = proxy_alloc_work(sock);
+		works[i] = quic_alloc_work(sock);
 	}
 
 	// Mqtt connect message
 	nng_msg *msg;
 	nng_mqtt_msg_alloc(&msg, 0);
 	nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
+
+	nng_mqtt_msg_set_connect_will_topic(msg, "topic");
+	char *willmsg = "will \n test";
+	nng_mqtt_msg_set_connect_will_msg(msg, willmsg, 12);
+	nng_mqtt_msg_set_connect_keep_alive(msg, 180);
+	nng_mqtt_msg_set_connect_clean_session(msg, true);
 
 	nng_mqtt_set_connect_cb(sock, connect_cb, sock_ret);
 	nng_mqtt_set_disconnect_cb(sock, disconnect_cb, NULL);
@@ -224,6 +531,7 @@ int
 quic_run()
 {
 	nng_socket sock;
+	// char * mqtt_url = "mqtt-tcp://10.10.201.160:1883";
 	char * mqtt_url = "mqtt-tcp://0.0.0.0:1883";
 	char * text = malloc(sizeof(char) * 20);
 
@@ -231,6 +539,8 @@ quic_run()
 
 	while (1) {
 		fgets(text, 20, stdin);
+		if (strncmp(text, "exit", 4) == 0)
+			exit(0);
 		client_publish(sock, "topic",
 		    (uint8_t *) text, strlen(text), 0, false);
 	}
